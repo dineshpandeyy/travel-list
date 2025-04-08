db0abf3b0f5f9f1eeabf3d031b6ad210ceb21e2a
dineshpandeyy <dineshpandey3536@gmail.com>
2025-04-08 00:31:47 -0500

    Controlled elements using there steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
   return (
     <div className="app">
       <h1>🗽 Far Away</h1>
-      <Logo />
+      <Form />
       <PackingList />
       <Stats />
     </div>
   );
 }
 
-function Logo() {
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!description) return 
+    const newItem = {quantity, description, packed:false, id:Date.now()} 
+    console.log(newItem)
+    setDescription("")
+    setQuantity(1)
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onClick={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) =>
+          <option value={num} key={num}>{num}</option>
+        )}
+      </select>
+      <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}></input>
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -29,7 +51,7 @@ function PackingList() {
       <ul>
         {
           initialItems.map((item) =>
-            <Item itemObj={item} />
+            <Item itemObj={item} key={item.id} />
           )
         }
       </ul>
@@ -40,7 +62,7 @@ function PackingList() {
 function Item({ itemObj }) {
   return (
     <li>
-      <span style={itemObj.packed ? {textDecoration: "line-through"} : {}}>
+      <span style={itemObj.packed ? { textDecoration: "line-through" } : {}}>
         {itemObj.quantity} {itemObj.description}
       </span>
       <button>❌</button>
