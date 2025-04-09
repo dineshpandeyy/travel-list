46126f1918c9c499512bcf6a3a9256f2c6d2ae97
dineshpandeyy <dineshpandey3536@gmail.com>
2025-04-08 23:39:03 -0500

    handel checkbox logic

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ export default function App() {
     setItemsAdded((items) => [...items, item])
   }
 
+  // You can also just pass id like on handleCheckBox and vice versa
   function handleDeleteItem(item) {
     setItemsAdded((items) => items.filter(it => it.id !== item.id))
+  }
+
+  function handleCheckBox(id) {
+    setItemsAdded((items) => items.map(it => it.id === id ? {...it, packed: !it.packed} : it))
   }
 
   return (
     <div className="app">
       <h1>🗽 Far Away</h1>
       <Form onHandleAddItem= {handleAddItem}/>
-      <PackingList itemsAdded= {itemsAdded} onHandleDeleteItem= {handleDeleteItem} />
+      <PackingList itemsAdded= {itemsAdded} onHandleDeleteItem= {handleDeleteItem}  onHandleCheckBox = {handleCheckBox}/>
       <Stats />
     </div>
   );
@@ -50,13 +55,13 @@ function Form( {onHandleAddItem} ) {
   );
 }
 
-function PackingList( {itemsAdded, onHandleDeleteItem}) {
+function PackingList( {itemsAdded, onHandleDeleteItem, onHandleCheckBox}) {
   return (
     <div className="list">
       <ul>
         {
           itemsAdded.map((item) =>
-            <Item itemObj={item} key={item.id} onHandleDeleteItem={onHandleDeleteItem} />
+            <Item itemObj={item} key={item.id} onHandleDeleteItem={onHandleDeleteItem} onHandleCheckBox = {onHandleCheckBox}/>
           )
         }
       </ul>
@@ -64,9 +69,10 @@ function PackingList( {itemsAdded, onHandleDeleteItem}) {
   )
 }
 
-function Item({ itemObj, onHandleDeleteItem}) {
+function Item({ itemObj, onHandleDeleteItem, onHandleCheckBox}) {
   return (
     <li>
+      <input type="checkbox" value={itemObj.packed} onClick={() => onHandleCheckBox(itemObj.id)}/>
       <span style={itemObj.packed ? { textDecoration: "line-through" } : {}}>
         {itemObj.quantity} {itemObj.description}
       </span>
