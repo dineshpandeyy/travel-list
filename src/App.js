553cc28bec523d464410f00f9316b9872ef9d145
dineshpandeyy <dineshpandey3536@gmail.com>
2025-04-09 00:57:49 -0500

    (imp) used the derived state to implement sorting feature (we use controlled elements using three steps)

--- src/App.js
+++ src/App.js
@@ -56,15 +56,41 @@ function Form({ onHandleAddItem }) {
 }
 
 function PackingList({ itemsAdded, onHandleDeleteItem, onHandleCheckBox }) {
+  const [sortBy, setSortBy] = useState("input")
+  let sortedItem;
+
+  if (sortBy === "input") sortedItem = itemsAdded;
+
+  if (sortBy === "description") {
+    sortedItem = itemsAdded
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItem = itemsAdded
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed)); // false (0) before true (1)
+  }
+
   return (
     <div className="list">
       <ul>
         {
-          itemsAdded.map((item) =>
+          sortedItem.map((item) =>
             <Item itemObj={item} key={item.id} onHandleDeleteItem={onHandleDeleteItem} onHandleCheckBox={onHandleCheckBox} />
           )
         }
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input Order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by Packed Status</option>
+        </select>
+
+      </div>
     </div>
   )
 }
